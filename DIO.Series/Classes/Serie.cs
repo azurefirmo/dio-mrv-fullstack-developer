9b9a742bdeb4414d7d48e9bd5decb2722a490969
Firmo Lopes de Oliveira Neto <azure_firmo@protonmail.com>
2022-01-22 23:10:04 -0300

    Criando um APP simples de cadastro de séries em .NET

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,21 @@
+using DIO.Series.Enum;
+
+namespace DIO.Series.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        private GeneroEnum Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Serie(int id, GeneroEnum genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+    }
+}
